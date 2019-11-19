214ddcd1c3ca8585a60c2cd5c501a325f1f39925
Ytemple <1911311017@qq.com>
2019-11-19 15:31:50 +0800

    enter

--- src/index.js
+++ src/index.js
@@ -8,5 +8,5 @@ const ICE_CONTAINER = document.getElementById('ice-container');
 if (!ICE_CONTAINER) {
   throw new Error('当前页面不存在 <div id="ice-container"></div> 节点.');
 }
-
+//223
 ReactDOM.render(router, ICE_CONTAINER);
